c0cc3f9c04c60ce13c772c083889ce7b4f8eb3ac
Sined0r <funfoxi4@mail.ru>
2026-02-27 16:16:43 +0300

    feat
    introduce Screw domain model with height constraints

---
+++ src/Screw.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TiltSystem
+{
+    public class Screw
+    {
+        private readonly double _minHeight;
+        private readonly double _maxHeight;
+
+        public double Height { get; private set; }
+
+        public Screw(double initialHeight, double minHeight, double maxHeight)
+        {
+            _minHeight = minHeight;
+            _maxHeight = maxHeight;
+            SetHeight(initialHeight);
+        }
+
+        public void Adjust(double delta)
+        {
+            SetHeight(Height + delta);
+        }
+
+        private void SetHeight(double value)
+        {
+            Height = Math.Clamp(value, _minHeight, _maxHeight);
+        }
+    }
+}
