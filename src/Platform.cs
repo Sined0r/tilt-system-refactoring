aa8e1543c10fc1e330177c7de3efe09086ebbe73
Sined0r <funfoxi4@mail.ru>
2026-02-27 16:30:11 +0300

    feat
    introduce Platform aggregate to encapsulate tilt system

---
+++ src/Platform.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TiltSystem
+{
+    public class Platform
+    {
+        public Screw Screw1 { get; }
+        public Screw Screw2 { get; }
+        public Screw Screw3 { get; }
+
+        private readonly double _radius;
+
+        public Platform(Screw screw1, Screw screw2, Screw screw3, double radius)
+        {
+            Screw1 = screw1;
+            Screw2 = screw2;
+            Screw3 = screw3;
+            _radius = radius;
+        }
+
+        public TiltResult CalculateTilt()
+        {
+            return TiltCalculator.Calculate(
+                Screw1.Height,
+                Screw2.Height,
+                Screw3.Height,
+                _radius
+            );
+        }
+    }
+}
